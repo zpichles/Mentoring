b84cb9104666b7468c7f6a89e769dd7b461c25f1
Ruslan <ruslan@MacBook-Air-Ruslan.local>
2024-04-25 17:53:00 +0200

    add task 1

---
+++ Ruslan/task1/JS/script.js
@@ -0,0 +1,4 @@
+const inputFirstNumber = parseFloat(prompt("Введите первое число")); //variable for first input number, i use parseFloat for transformation string to float
+const inputSecondNumber = parseFloat(prompt("Введите второе число")); //variable for second input number
+let sumOfNumbers = inputFirstNumber + inputSecondNumber; //sum two input numbers
+console.log("Сумма :", sumOfNumbers); //output data to console
