ffc643bd0aa6fbf585f62121782520b3e871c357
Andrey Stoletov <98499546+theRacoonPrime@users.noreply.github.com>
2024-04-25 18:54:42 +0200

    Update script.js

--- Ruslan/task2/JS/script.js
+++ Ruslan/task2/JS/script.js
@@ -3,3 +3,5 @@ function isEven (number) { //function testing number is even or odd
     return number % 2 === 0; //checking the modulus after dividing a number by 2
     }
 console.log(isEven (inputNumber)); //output to console
+
+// Try to figure out why do you need use float  not int and why ?  Can you use int ? 
