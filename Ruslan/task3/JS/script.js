e5b9a847badee4883d682c4e2fc7c8f82ce4ddff
Blablabla585 <mamedovruslan30@gmail.com>
2024-04-28 19:21:37 +0300

    add task3

---
+++ Ruslan/task3/JS/script.js
@@ -0,0 +1,10 @@
+const num1 = parseFloat(prompt("Введите число №1")); //variable for first input number, i use parseFloat for transformation string to float
+const num2 = parseFloat(prompt("Введите число №2")); //variable for second input number
+const num3 = parseFloat(prompt("Введите число №3")); //variable for third input number
+if (num1 > num2 && num1 > num3) { //compare num1, num2 and num3
+    console.log(num1); //if num1 is largest, it will be displayed
+} else if (num2 > num1 && num2 > num3) { //compare num1, num2 and num3
+    console.log(num2); //if num2 is largest, it will be displayed
+} else if (num3 > num1 && num3 > num2) { //compare num1, num2 and num3
+    console.log(num3); //if num3 is largest, it will be displayed
+}
