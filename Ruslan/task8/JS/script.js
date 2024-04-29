19f0ca43d0c869bbf5f951a6127bba626b457971
Blablabla585 <mamedovruslan30@gmail.com>
2024-04-30 00:00:55 +0300

    add task 8

---
+++ Ruslan/task8/JS/script.js
@@ -0,0 +1,8 @@
+var a = 7;
+var b = 5;
+if (a > b) { //condition, if b is less than a
+    var x = a + b;
+} else { //condition, if a and b are equal or if a is less than b
+    var x = a * b;
+}
+console.log(x); //display value in console
