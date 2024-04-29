02782f341de92170fc3a1cf81683370d286defd5
Blablabla585 <mamedovruslan30@gmail.com>
2024-04-29 23:51:48 +0300

    add task 7

---
+++ Ruslan/task7/JS/script.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = 5;
+var prod = a * b; //product of a and b
+var sum = a + b; //sum of a and b
+var x = 2 * (prod % sum); //remainder of the division product by sum
+console.log(x); //display value in console
