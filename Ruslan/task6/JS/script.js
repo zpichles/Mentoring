b8a4213e8980c9ce7257ac1d08b098b276646219
Andrey Stoletov <98499546+theRacoonPrime@users.noreply.github.com>
2024-05-04 23:12:04 +0200

    Update script.js

--- Ruslan/task6/JS/script.js
+++ Ruslan/task6/JS/script.js
@@ -3,3 +3,13 @@ var b = 3;
 var x = a + b;
 console.log(x); //display value in console
 
+//  The same problem , you are not understand it a little bit , and because of that you did only the part of task . Code should look like bellow 
+// function somethingA(a, b) {
+//   var x = a + b;
+//   return x;
+// }
+
+// var a = 2;
+// var b = 3;
+// var x = testA(a, b);
+// console.log(x); // Display value in console
