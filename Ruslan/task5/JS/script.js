0815e972b80cc9298395080b1e0b43c46804ed00
Andrey Stoletov <98499546+theRacoonPrime@users.noreply.github.com>
2024-05-04 23:02:35 +0200

    Update script.js

--- Ruslan/task5/JS/script.js
+++ Ruslan/task5/JS/script.js
@@ -3,3 +3,8 @@ var a = 10; //make variable x with value 10
 var x = a; // assigning a to a variable x
 console.log(x); //display value in console
 
+//  there some issue , you not deeply understand what you should to do , you can do it like this for instance 
+// function testA(a) {
+//   var x = a;
+//   return x;
+// }
