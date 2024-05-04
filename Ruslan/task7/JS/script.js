00f3c6a0a2e5072768996b3a68d91a852fe49870
Andrey Stoletov <98499546+theRacoonPrime@users.noreply.github.com>
2024-05-04 23:15:41 +0200

    Update script.js

--- Ruslan/task7/JS/script.js
+++ Ruslan/task7/JS/script.js
@@ -4,3 +4,5 @@ var prod = a * b; //product of a and b
 var sum = a + b; //sum of a and b
 var x = 2 * (prod % sum); //remainder of the division product by sum
 console.log(x); //display value in console
+
+//  It is good , nice job ,  go ahead 
