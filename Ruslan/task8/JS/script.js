510cf4b5bd37324a72f43ff859903547e893e4bc
Andrey Stoletov <98499546+theRacoonPrime@users.noreply.github.com>
2024-05-04 23:19:11 +0200

    Update script.js

--- Ruslan/task8/JS/script.js
+++ Ruslan/task8/JS/script.js
@@ -6,3 +6,16 @@ if (a > b) { //condition, if b is less than a
     var x = a * b;
 }
 console.log(x); //display value in console
+
+//  Bassicaly good , but you can improve it  try to make it like that 
+// var a = 7;
+// var b = 5;
+// var x;
+
+// if (a > b) {
+//     x = a + b;
+// } else {
+//     x = a * b;
+// }
+
+// console.log(x); 
