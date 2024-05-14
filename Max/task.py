828ca75c13fd9501cf41569d37ac827dedcda5f6
zpichles <168102098+zpichles@users.noreply.github.com>
2024-05-14 21:25:14 +0200

    Update task.py

--- Max/task.py
+++ Max/task.py
@@ -1,1 +1,132 @@
 print(" 4\n 8\n 15\n 16\n 23\n 424\n 8\n 15\n 16\n 23\n 42")
+Task4
+print(" *\n **\n ***\n ****\n *****\n ******\n *******")
+
+
+
+
+task5
+print("Hello, ", input())
+
+
+
+
+task6
+print(input(), "- champion")
+
+
+
+task7
+name1=input("enter first variable:")
+name2=input("enter second variable")
+name3=input("enter third variable")
+print("first variable is ", name1)
+print("second variable is ",name2)
+print (name3, "is the 3 variable")
+
+
+
+
+task8
+name1=input("enter first variable:")
+name2=input("enter second variable")
+name3=input("enter third variable")
+print("third variable is ", name3)
+print("second variable is ",name2)
+print("first variable", name1)
+task8*
+print("I***like***Python")
+
+
+
+
+
+task9
+print("Hello,",input(), end="!")
+
+
+
+
+task10
+divider=input()#selector input
+input1=input()
+input2=input()
+input3=input()
+print(input1, input2, input3, sep=divider)
+
+
+
+
+task11
+a=int(input())
+b=a+1
+c=b+1
+print(a)
+print(b)
+print(c)
+
+
+
+
+
+task12
+a=int(input())
+b=int(input())
+c=int(input())
+d=a+b+c
+print(d)
+
+
+
+
+
+task13
+print("goni rebro")
+h=int(input())
+S=6*h*h
+V=h*h*h
+print(S, "square")
+print(V, "volume")
+
+
+
+
+task14
+print("a,b")
+a=int(input())
+b=int(input())
+f=3*(a+b)*(a+b)*(a+b)+275*b*b-127*a-41
+print("3*(a+b)*(a+b)*(a+b)+275*b*b-127*a-41 =",f )
+
+
+
+task15
+a=int(input())
+b=a-1
+c=b-1
+print("origin number",a)
+print("previous number",b)
+print("preprevious number",c)
+
+
+
+task16
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+sum=(a+b+c+d)*3
+print(sum)
+
+
+task17
+a=int(input())
+b=int(input())
+c=int(input())
+d=(a+b+c)*2
+print(d)f
+
+
+
+
+
